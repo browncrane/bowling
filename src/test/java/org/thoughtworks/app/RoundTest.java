a736f520bd9d80cf2e29dab46a7402ba48bc5a5a
browncrane <im2busy@qq.com>
2013-04-18 15:31:41 +0800

    refactor for task 5.

--- src/test/java/org/thoughtworks/app/RoundTest.java
+++ src/test/java/org/thoughtworks/app/RoundTest.java
@@ -1,5 +1,6 @@
 package org.thoughtworks.app;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -11,22 +12,28 @@ import static org.hamcrest.core.Is.is;
  * Time: 下午8:30
  */
 public class RoundTest {
+
+    private Round normal;
+    private Round spare;
+
+    @Before
+    public void SetUp() {
+        int hitFour = 4;
+        int hitOne = 1;
+        int hitFive = 5;
+        normal = new Round(hitOne, hitFour);
+        spare = new Round(hitFive, hitFive);
+    }
+
     @Test
     public void should_return_the_sum_of_two_hits() {
-        int firstHit = 1;
-        int secondHit = 4;
-        Round round = new Round(firstHit, secondHit);
-        assertThat(round.hitDownAll(), is(5));
+        assertThat(normal.hitDownAll(), is(5));
+        assertThat(spare.hitDownAll(), is(10));
     }
 
     @Test
     public void should_return_true_if_the_round_is_spare() throws Exception {
-        int firstHit = 5;
-        int secondHit = 5;
-        int hitThree = 3;
-        Round spareRound = new Round(firstHit, secondHit);
-        assertThat(spareRound.isSpare(),is(true));
-        Round normalRound = new Round(firstHit,hitThree);
-        assertThat(normalRound.isSpare(),is(false));
+        assertThat(spare.isSpare(), is(true));
+        assertThat(normal.isSpare(), is(false));
     }
 }
